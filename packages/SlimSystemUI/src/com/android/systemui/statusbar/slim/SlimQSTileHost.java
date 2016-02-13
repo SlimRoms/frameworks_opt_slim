c57a87552e1abd37ce3c5da308d36b6c78b452c7
Alex Cruz <mazdarider23@gmail.com>
2016-02-12 21:44:17 -0500

    Add IME selector QS Tile
    See image - http://i.imgur.com/83vHFI0.png
    
    Change-Id: I48fd585901cecd24b0ba94409e69b6a5cda907b1
    Signed-off-by: Josue Rivera <prbassplayer@gmail.com>

--- packages/SlimSystemUI/src/com/android/systemui/statusbar/slim/SlimQSTileHost.java
+++ packages/SlimSystemUI/src/com/android/systemui/statusbar/slim/SlimQSTileHost.java
@@ -25,6 +25,7 @@ import com.android.systemui.statusbar.policy.ZenModeController;
 
 import com.android.systemui.qs.tiles.CaffeineTile;
 import com.android.systemui.qs.tiles.ExpandedDesktopTile;
+import com.android.systemui.qs.tiles.ImeTile;
 import com.android.systemui.qs.tiles.SyncTile;
 
 public class SlimQSTileHost extends QSTileHost {
@@ -52,6 +53,8 @@ public class SlimQSTileHost extends QSTileHost {
                 return new CaffeineTile(this);
             case "expanded_desktop":
                 return new ExpandedDesktopTile(this);
+            case "ime":
+                return new ImeTile(this);
             case "sync":
                 return new SyncTile(this);
             default:
