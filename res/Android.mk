8d26d8132a4944cd8d4f4b2c706e882ea7d94abf
Daniel Koman <dankoman30@gmail.com>
2016-06-26 00:50:57 +0200

    temp fixes
    Change-Id: I3a24af1a200f8a0dbd5b251d97a60ea2bbc7524b

--- res/Android.mk
+++ res/Android.mk
@@ -4,6 +4,12 @@ include $(CLEAR_VARS)
 #LOCAL_NO_STANDARD_LIBRARIES := true
 LOCAL_PACKAGE_NAME := org.slim.framework-res
 LOCAL_CERTIFICATE := platform
+LOCAL_AAPT_FLAGS := --auto-add-overlay
+LOCAL_RESOURCE_DIR := $(addprefix $(LOCAL_PATH)/, res)
+
+# Tell aapt to create "extending (non-application)" resource IDs,
+# since these resources will be used by many apps.
+LOCAL_AAPT_FLAGS += -x
 
 LOCAL_MODULE_TAGS := optional
 
