91dde58f6455cf91857d30b5a979a6e511f18eba
Nicholas Flintham <nick@flinny.org>
2016-07-04 15:27:18 +0100

    Add Advanced settings to SlimMetricsLogger
    Change-Id: I0b4f4f70ac44a7d40bfe15c5a1d45a21deef8864

--- src/org/slim/framework/internal/logging/SlimMetricsLogger.java
+++ src/org/slim/framework/internal/logging/SlimMetricsLogger.java
@@ -32,4 +32,5 @@ public class SlimMetricsLogger extends MetricsLogger {
     public static final int NAV_BAR_DIMEN_SETTINGS = BASE + 6;
     public static final int NAVIGATION_SETTINGS = BASE + 7;
     public static final int HARDWAREKEYS_SETTINGS = BASE + 8;
+    public static final int ADVANCED_SETTINGS = BASE + 9;
 }
