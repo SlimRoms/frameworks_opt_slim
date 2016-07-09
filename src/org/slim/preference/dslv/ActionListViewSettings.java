98ccec61c1676000f554236724608a84ed72974f
Jean-Pierre Rasquin <yank555.lu@gmail.com>
2016-07-09 17:52:52 +0200

    NavBar/dslv: Allow "None" for doubletap actions
    Else there is no easy way to remove a doubletap action other than by removing the button and settings it up from scratch again
    
    Change-Id: Ied7a4d9a76c233b2bf3f7de9dfd95076a96be7e4
    Signed-off-by: Jean-Pierre Rasquin <yank555.lu@gmail.com>

--- src/org/slim/preference/dslv/ActionListViewSettings.java
+++ src/org/slim/preference/dslv/ActionListViewSettings.java
@@ -883,7 +883,7 @@ public class ActionListViewSettings extends ListFragment implements
                     // on normal press action
                     String[] values = null;
                     String[] entries = null;
-                    if (!longpress) {
+                    if (!longpress && !doubletap) {
                         List<String> finalEntriesList = new ArrayList<String>();
                         List<String> finalValuesList = new ArrayList<String>();
 
@@ -900,9 +900,9 @@ public class ActionListViewSettings extends ListFragment implements
                     }
 
                     final String[] finalDialogValues =
-                        longpress ? getOwner().mActionDialogValues : values;
+                        (longpress || doubletap) ? getOwner().mActionDialogValues : values;
                     final String[] finalDialogEntries =
-                        longpress ? getOwner().mActionDialogEntries : entries;
+                        (longpress || doubletap) ? getOwner().mActionDialogEntries : entries;
 
                     return new AlertDialog.Builder(getActivity())
                     .setTitle(getOwner().getContext().getString(title))
