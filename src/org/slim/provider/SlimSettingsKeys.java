d0180d7d243be3081429b6ae0d76f374edb9096f
Jean-Pierre Rasquin <yank555.lu@gmail.com>
2016-07-26 22:52:22 +0200

    ﻿[3/3] Slim: Code: QS Tile customizations
    Change-Id: I47ba368091a2f0ffac38b65988af849ee6f00abb
    Signed-off-by: Jean-Pierre Rasquin <yank555.lu@gmail.com>

--- src/org/slim/provider/SlimSettingsKeys.java
+++ src/org/slim/provider/SlimSettingsKeys.java
@@ -492,6 +492,19 @@ public final class SlimSettingsKeys {
          */
         public static final String STATUS_BAR_BATTERY_PERCENT = "status_bar_battery_percent";
 
+        /**
+         * Whether to show the brightness slider in quick settings panel.
+         * @hide
+         */
+        public static final String QS_SHOW_BRIGHTNESS_SLIDER = "qs_show_brightness_slider";
+
+        /**
+         * Number of columns to display on the quick settings panel
+         * Default is 3
+         * @hide
+         */
+        public static final String QS_NUM_TILE_COLUMNS = "qs_num_tile_columns";
+
     }
 
     public interface Global {
