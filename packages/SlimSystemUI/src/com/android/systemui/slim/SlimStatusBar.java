44b90ae90510d497e19711d4dbde35ded4041a47
Griffin Millender <griffinn.millender@gmail.com>
2016-07-30 14:45:14 -0500

    Add SlimNavigationBar to SlimSystemUI
    Change-Id: I294c1952afce6b5ab4b9a85be4a15331accbc88e

--- packages/SlimSystemUI/src/com/android/systemui/slim/SlimStatusBar.java
+++ packages/SlimSystemUI/src/com/android/systemui/slim/SlimStatusBar.java
@@ -1,6 +1,294 @@
 package com.android.systemui.statusbar.slim;
 
+import android.app.ActivityManager;
+import android.app.ActivityManagerNative;
+import android.app.IActivityManager;
+import android.content.ContentResolver;
+import android.database.ContentObserver;
+import android.graphics.PixelFormat;
+import android.net.Uri;
+import android.os.Handler;
+import android.os.RemoteException;
+import android.os.UserHandle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup.LayoutParams;
+import android.view.WindowManager;
+
+import com.android.systemui.R;
+import com.android.systemui.recents.ScreenPinningRequest;
+import com.android.systemui.statusbar.phone.NavigationBarView;
 import com.android.systemui.statusbar.phone.PhoneStatusBar;
+import com.android.systemui.statusbar.phone.PhoneStatusBarView;
+
+import org.slim.provider.SlimSettings;
 
 public class SlimStatusBar extends PhoneStatusBar {
+
+    private static final String TAG = "SlimStatusBar";
+
+    private PhoneStatusBarView mStatusBarView;
+    private SlimNavigationBarView mSlimNavigationBarView;
+
+    private boolean mHasNavigationBar;
+
+    private long mLastLockToAppLongPress;
+
+    private ScreenPinningRequest.ScreenPinningCallback mScreenPinningCallback =
+            new ScreenPinningRequest.ScreenPinningCallback() {
+        @Override
+        public void onStartLockTask() {
+            mSlimNavigationBarView.setOverrideMenuKeys(true);
+        }
+    };
+
+    private SlimKeyButtonView.LongClickCallback mLongClickCallback =
+            new SlimKeyButtonView.LongClickCallback() {
+        @Override
+        public boolean onLongClick(View v) {
+            return handleLongPressBackRecents(v);
+        }
+    };
+
+    class SettingsObserver extends ContentObserver {
+        SettingsObserver(Handler handler) {
+            super(handler);
+        }
+
+        void observe() {
+            ContentResolver resolver = mContext.getContentResolver();
+            resolver.registerContentObserver(SlimSettings.System.getUriFor(
+                    SlimSettings.System.NAVIGATION_BAR_BUTTON_TINT),
+                    false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(SlimSettings.System.getUriFor(
+                    SlimSettings.System.NAVIGATION_BAR_BUTTON_TINT_MODE),
+                    false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(SlimSettings.System.getUriFor(
+                    SlimSettings.System.NAVIGATION_BAR_GLOW_TINT),
+                    false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(SlimSettings.System.getUriFor(
+                    SlimSettings.System.NAVIGATION_BAR_SHOW),
+                    false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(SlimSettings.System.getUriFor(
+                    SlimSettings.System.NAVIGATION_BAR_CONFIG),
+                    false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(SlimSettings.System.getUriFor(
+                    SlimSettings.System.NAVIGATION_BAR_CAN_MOVE),
+                    false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(SlimSettings.System.getUriFor(
+                    SlimSettings.System.MENU_LOCATION),
+                    false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(SlimSettings.System.getUriFor(
+                    SlimSettings.System.MENU_VISIBILITY),
+                    false, this, UserHandle.USER_ALL);
+        }
+
+        @Override
+        public void onChange(boolean selfChange, Uri uri) {
+            super.onChange(selfChange, uri);
+
+            if (uri.equals(SlimSettings.System.getUriFor(
+                    SlimSettings.System.NAVIGATION_BAR_BUTTON_TINT))
+                || uri.equals(SlimSettings.System.getUriFor(
+                    SlimSettings.System.NAVIGATION_BAR_BUTTON_TINT_MODE))
+                || uri.equals(SlimSettings.System.getUriFor(
+                    SlimSettings.System.NAVIGATION_BAR_CONFIG))
+                || uri.equals(SlimSettings.System.getUriFor(
+                    SlimSettings.System.NAVIGATION_BAR_GLOW_TINT))
+                || uri.equals(SlimSettings.System.getUriFor(
+                    SlimSettings.System.MENU_LOCATION))
+                || uri.equals(SlimSettings.System.getUriFor(
+                    SlimSettings.System.MENU_VISIBILITY))) {
+                if (mSlimNavigationBarView != null) {
+                    mSlimNavigationBarView.recreateNavigationBar();
+                    prepareNavigationBarView();
+                }
+            } else if (uri.equals(SlimSettings.System.getUriFor(
+                    SlimSettings.System.NAVIGATION_BAR_CAN_MOVE))) {
+                prepareNavigationBarView();
+            } else if (uri.equals(SlimSettings.System.getUriFor(
+                    SlimSettings.System.NAVIGATION_BAR_SHOW))) {
+                updateNavigationBarVisibility();
+            }
+        }
+    }
+
+    @Override
+    public void start() {
+        super.start();
+
+        updateNavigationBarVisibility();
+
+        SettingsObserver observer = new SettingsObserver(mHandler);
+        observer.observe();
+
+        mScreenPinningRequest.setCallback(mScreenPinningCallback);
+    }
+
+    @Override
+    protected PhoneStatusBarView makeStatusBarView() {
+        mStatusBarView = super.makeStatusBarView();
+
+        Log.d(TAG, "makeStatusBarView");
+
+        if (mSlimNavigationBarView == null) {
+            mSlimNavigationBarView = (SlimNavigationBarView)
+                    View.inflate(mContext, R.layout.slim_navigation_bar, null);
+        }
+
+        mSlimNavigationBarView.setDisabledFlags(mDisabled1);
+        mSlimNavigationBarView.setBar(this);
+        mSlimNavigationBarView.setOnVerticalChangedListener(
+                new NavigationBarView.OnVerticalChangedListener() {
+            @Override
+            public void onVerticalChanged(boolean isVertical) {
+                if (mAssistManager != null) {
+                    mAssistManager.onConfigurationChanged();
+                }
+                mNotificationPanel.setQsScrimEnabled(!isVertical);
+            }
+        });
+        mSlimNavigationBarView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                checkUserAutohide(v, event);
+                return false;
+            }
+        });
+
+        if (mNavigationBarView != null) {
+            if (mNavigationBarView.isAttachedToWindow()) {
+                try {
+                    mWindowManager.removeView(mNavigationBarView);
+                } catch (Exception e) {}
+            }
+            mNavigationBarView = mSlimNavigationBarView;
+        }
+
+        return mStatusBarView;
+    }
+
+    private void updateNavigationBarVisibility() {
+        final int showByDefault = mContext.getResources().getBoolean(
+                    com.android.internal.R.bool.config_showNavigationBar) ? 1 : 0;
+        mHasNavigationBar = SlimSettings.System.getIntForUser(mContext.getContentResolver(),
+                    SlimSettings.System.NAVIGATION_BAR_SHOW, showByDefault,
+                    UserHandle.USER_CURRENT) == 1;
+
+        if (mHasNavigationBar) {
+            addNavigationBar();
+        } else {
+            if (mSlimNavigationBarView.isAttachedToWindow()) {
+                mWindowManager.removeView(mSlimNavigationBarView);
+            }
+        }
+    }
+
+    private void prepareNavigationBarView() {
+        mSlimNavigationBarView.reorient();
+
+        View home = mSlimNavigationBarView.getHomeButton();
+        View recents = mSlimNavigationBarView.getRecentsButton();
+
+        mSlimNavigationBarView.setPinningCallback(mLongClickCallback);
+
+        /*if (recents != null) {
+            recents.setOnClickListener(mRecentsClickListener);
+            recents.setOnTouchListener(mRecentsPreloadOnTouchListener);
+        }
+        if (home != null) {
+            home.setOnTouchListener(mHomeActionListener);
+        }*/
+
+        mAssistManager.onConfigurationChanged();
+    }
+
+    private void addNavigationBar() {
+        if (DEBUG) Log.v(TAG, "addNavigationBar: about to add " + mSlimNavigationBarView);
+        if (mSlimNavigationBarView == null) return;
+
+        prepareNavigationBarView();
+
+        if (!mSlimNavigationBarView.isAttachedToWindow()) {
+            try {
+                mWindowManager.addView(mSlimNavigationBarView, getNavigationBarLayoutParams());
+            } catch (Exception e) {}
+        }
+    }
+
+    private void repositionNavigationBar() {
+        if (mSlimNavigationBarView == null
+                || !mSlimNavigationBarView.isAttachedToWindow()) return;
+
+        prepareNavigationBarView();
+
+        mWindowManager.updateViewLayout(mSlimNavigationBarView, getNavigationBarLayoutParams());
+    }
+
+    private WindowManager.LayoutParams getNavigationBarLayoutParams() {
+        WindowManager.LayoutParams lp = new WindowManager.LayoutParams(
+                LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT,
+                WindowManager.LayoutParams.TYPE_NAVIGATION_BAR,
+                    0
+                    | WindowManager.LayoutParams.FLAG_TOUCHABLE_WHEN_WAKING
+                    | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+                    | WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
+                    | WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH
+                    | WindowManager.LayoutParams.FLAG_SPLIT_TOUCH,
+                PixelFormat.TRANSLUCENT);
+        // this will allow the navbar to run in an overlay on devices that support this
+        if (ActivityManager.isHighEndGfx()) {
+            lp.flags |= WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED;
+        }
+
+        lp.setTitle("NavigationBar");
+        lp.windowAnimations = 0;
+        return lp;
+    }
+
+    private boolean handleLongPressBackRecents(View v) {
+        try {
+            boolean sendBackLongPress = false;
+            IActivityManager activityManager = ActivityManagerNative.getDefault();
+            boolean isAccessiblityEnabled = mAccessibilityManager.isEnabled();
+            if (activityManager.isInLockTaskMode() && !isAccessiblityEnabled) {
+                // If we recently long-pressed the other button then they were
+                // long-pressed 'together'
+                if (mSlimNavigationBarView.getRightMenuButton().isPressed()
+                        && mSlimNavigationBarView.getLeftMenuButton().isPressed()) {
+                    activityManager.stopLockTaskModeOnCurrent();
+                    // When exiting refresh disabled flags.
+                    mSlimNavigationBarView.setDisabledFlags(mDisabled1, true);
+                    mSlimNavigationBarView.setOverrideMenuKeys(false);
+                } else if ((v.getId() == mSlimNavigationBarView.getLeftMenuButton().getId())
+                        && !mSlimNavigationBarView.getRightMenuButton().isPressed()) {
+                    // If we aren't pressing recents right now then they presses
+                    // won't be together, so send the standard long-press action.
+                    sendBackLongPress = true;
+                }
+            } else {
+                // If this is back still need to handle sending the long-press event.
+                long time = System.currentTimeMillis();
+                if (( time - mLastLockToAppLongPress) < 2000) {
+                    if (v.getId() == mSlimNavigationBarView.getLeftMenuButton().getId()
+                        || v.getId() == mSlimNavigationBarView.getRightMenuButton().getId()) {
+                        sendBackLongPress = true;
+                    }
+                } else if (isAccessiblityEnabled && activityManager.isInLockTaskMode()) {
+                    // When in accessibility mode a long press that is recents (not back)
+                    // should stop lock task.
+                    activityManager.stopLockTaskModeOnCurrent();
+                    // When exiting refresh disabled flags.
+                    mSlimNavigationBarView.setDisabledFlags(mDisabled1, true);
+                    mSlimNavigationBarView.setOverrideMenuKeys(false);
+                }
+                mLastLockToAppLongPress = time;
+            }
+            return sendBackLongPress;
+        } catch (RemoteException e) {
+            Log.d(TAG, "Unable to reach activity manager", e);
+            return false;
+        }
+    }
 }
