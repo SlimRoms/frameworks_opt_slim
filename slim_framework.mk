4b2ba40b04ae6cd14e09229e4eab4d6339e10997
Griffin Millender <griffinn.millender@gmail.com>
2016-08-12 23:10:52 -0500

    Add SlimPermissions package to handle Permissions dialog and Permissions settings page
    Change-Id: Ie0d7da3c6364eba553510ba5b0f9104084dfc9c8

--- slim_framework.mk
+++ slim_framework.mk
@@ -2,7 +2,8 @@ PRODUCT_PACKAGES += \
     org.slim.framework \
     org.slim.framework-res \
     SlimSettingsProvider \
-    SlimSettings
+    SlimSettings \
+    SlimPermissionHandler
 
 PRODUCT_BOOT_JARS += \
     org.slim.framework
