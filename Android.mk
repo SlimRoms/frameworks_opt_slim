2eb43c22ee220d74422124bc69a5727c29a4c185
Griffin Millender <griffinn.millender@gmail.com>
2016-08-16 19:16:03 -0500

    Rework slim settings
    Change-Id: I69cda806a422d086ab04670897998e656cc04169

--- Android.mk
+++ Android.mk
@@ -22,6 +22,8 @@ LOCAL_SRC_FILES := $(call all-java-files-under,src)
 
 LOCAL_SRC_FILES += $(call all-Iaidl-files-under,src)
 
+LOCAL_SHARED_ANDROID_LIBRARIES += android-support-v7-preference
+
 slim_platform_res := APPS/org.slim.framework-res_intermediates/src
 
 LOCAL_INTERMEDIATE_SOURCES := \
