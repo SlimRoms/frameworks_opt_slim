8765ac80d99d86c27f84c6d5420b59ebc0f60f0d
Daniel Koman <dankoman30@gmail.com>
2016-10-15 19:18:06 +0200

    (1/2) longpress status bar header for notification log
    Conflicts:
    	packages/SlimSystemUI/src/com/android/systemui/statusbar/slim/SlimQuickStatusBarHeader.java
    	packages/SlimSystemUI/src/com/android/systemui/statusbar/slim/SlimStatusBar.java
    
    Change-Id: I88c6ed93b98a8a865ae089b664e80e0365df4646

--- packages/SlimSystemUI/src/com/android/systemui/statusbar/slim/SlimStatusBar.java
+++ packages/SlimSystemUI/src/com/android/systemui/statusbar/slim/SlimStatusBar.java
@@ -55,6 +55,7 @@ import com.android.systemui.statusbar.phone.SlimNavigationBarView;
 import com.android.systemui.statusbar.phone.NavigationBarView;
 import com.android.systemui.statusbar.phone.PhoneStatusBar;
 import com.android.systemui.statusbar.phone.PhoneStatusBarView;
+import com.android.systemui.statusbar.slim.SlimQuickStatusBarHeader;
 
 import java.util.List;
 
@@ -91,6 +92,8 @@ public class SlimStatusBar extends PhoneStatusBar implements
     private boolean mHasNavigationBar = false;
     private boolean mNavigationBarAttached = false;
     private boolean mDisableHomeLongpress = false;
+
+    private SlimQuickStatusBarHeader mSlimQuickStatusBarHeader;
 
     class SettingsObserver extends ContentObserver {
         SettingsObserver(Handler handler) {
@@ -279,6 +282,21 @@ public class SlimStatusBar extends PhoneStatusBar implements
         if (mNavigationBarView != mSlimNavigationBarView) {
             mNavigationBarView = mSlimNavigationBarView;
         }
+
+        mSlimQuickStatusBarHeader = (SlimQuickStatusBarHeader) mHeader;
+        mSlimQuickStatusBarHeader.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_MAIN);
+                intent.setClassName("com.android.settings",
+                        "com.android.settings.Settings$NotificationStationActivity");
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                mSlimQuickStatusBarHeader.getActivityStarter().startActivity(
+                        intent, true /* dismissShade */);
+                return true;
+            }
+        });
+
     }
 
     private void updateNavigationBarVisibility() {
