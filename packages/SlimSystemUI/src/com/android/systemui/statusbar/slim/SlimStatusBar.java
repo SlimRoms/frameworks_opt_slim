3a6d1301342a7b7c54ff5b3c1db347e3eeea09ba
Daniel Koman <dankoman30@gmail.com>
2016-10-28 15:00:56 +0200

    (1/2) fix profile switching
    move secret notification log switch to header instead of button
    
    Change-Id: Ia2b940236b708a2adb26ad1b8a110d5ca6c5ddb5

--- packages/SlimSystemUI/src/com/android/systemui/statusbar/slim/SlimStatusBar.java
+++ packages/SlimSystemUI/src/com/android/systemui/statusbar/slim/SlimStatusBar.java
@@ -35,7 +35,6 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup.LayoutParams;
 import android.view.WindowManager;
-import android.widget.ImageView;
 
 import com.android.systemui.R;
 import com.android.systemui.slimrecent.RecentController;
@@ -43,6 +42,7 @@ import com.android.systemui.statusbar.phone.SlimNavigationBarView;
 import com.android.systemui.statusbar.phone.NavigationBarView;
 import com.android.systemui.statusbar.phone.PhoneStatusBar;
 import com.android.systemui.statusbar.phone.PhoneStatusBarView;
+import com.android.systemui.statusbar.slim.SlimStatusBarHeaderView;
 
 import org.slim.provider.SlimSettings;
 
@@ -69,7 +69,7 @@ public class SlimStatusBar extends PhoneStatusBar {
     private boolean mNavigationBarAttached = false;
     private boolean mDisableHomeLongpress = false;
 
-    private ImageView mMultiUserAvatar;
+    private SlimStatusBarHeaderView mSlimStatusBarHeaderView;
 
     class SettingsObserver extends ContentObserver {
         SettingsObserver(Handler handler) {
@@ -248,16 +248,16 @@ public class SlimStatusBar extends PhoneStatusBar {
 
         mSlimIconController = new SlimStatusBarIconController(mContext, mStatusBarView, this);
 
-        mMultiUserAvatar = (ImageView) mHeader.findViewById(R.id.multi_user_avatar);
-        mMultiUserAvatar.setOnLongClickListener(new View.OnLongClickListener() {
+        mSlimStatusBarHeaderView = (SlimStatusBarHeaderView) mHeader;
+        mSlimStatusBarHeaderView.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
                 Intent intent = new Intent(Intent.ACTION_MAIN);
                 intent.setClassName("com.android.settings",
                         "com.android.settings.Settings$NotificationStationActivity");
                 intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                animateCollapsePanels();
-                mContext.startActivityAsUser(intent, new UserHandle(UserHandle.USER_CURRENT));
+                mSlimStatusBarHeaderView.getActivityStarter().startActivity(
+                        intent, true /* dismissShade */);
                 return true;
             }
         });
