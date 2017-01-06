ad70f59b982194c059232d04b7d34bc6914d9713
Griffin Millender <griffinn.millender@gmail.com>
2017-01-05 21:41:48 -0600

    Cleanup and fix ColorPickerPreference
    Change-Id: I267f944114b1d6b54c0192727e904419f9c88e2b

--- preference/Android.mk
+++ preference/Android.mk
@@ -16,11 +16,24 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
+LOCAL_USE_AAPT2 := true
+LOCAL_MODULE := slim-preference-res
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+LOCAL_JAVA_LANGUAGE_VERSION := 1.7
+include $(BUILD_STATIC_JAVA_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_USE_AAPT2 := true
 LOCAL_MODULE := slim-preference
 LOCAL_SRC_FILES := $(call all-java-files-under,src)
-LOCAL_JAVA_LIBRARIES := \
-    org.slim.framework \
+LOCAL_STATIC_ANDROID_LIBRARIES := \
+    slim-preference-res
+LOCAL_SHARED_ANDROID_LIBRARIES := \
     android-support-v7-preference \
     android-support-v7-recyclerview \
     android-support-v14-preference
+LOCAL_JAVA_LIBRARIES := \
+    org.slim.framework
+LOCAL_JAR_EXCLUDE_FILES := none
+LOCAL_JAVA_LANGUAGE_VERSION := 1.7
 include $(BUILD_STATIC_JAVA_LIBRARY)
