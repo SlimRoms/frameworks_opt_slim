283ee8fbd0aa83c0865159f978e44b69cfab9e27
Griffin Millender <griffinn.millender@gmail.com>
2017-01-17 22:07:39 -0600

    Fix Multi User for SlimSettings
    Change-Id: Ia1725ae22e38a02a27d420939c50d83ee4eac3ef

--- packages/SlimSystemUI/src/com/android/systemui/slimrecent/RecentController.java
+++ packages/SlimSystemUI/src/com/android/systemui/slimrecent/RecentController.java
@@ -64,6 +64,7 @@ import com.android.systemui.recents.misc.Utilities;
 import com.android.systemui.statusbar.BaseStatusBar;
 
 import slim.provider.SlimSettings;
+import slim.utils.UserContentObserver;
 
 /**
  * Our main recents controller.
@@ -546,12 +547,14 @@ public class RecentController implements RecentPanelView.OnExitListener,
      * Settingsobserver to take care of the user settings.
      * Either gravity or scale factor of our recent panel can change.
      */
-    private class SettingsObserver extends ContentObserver {
+    private class SettingsObserver extends UserContentObserver {
         SettingsObserver(Handler handler) {
             super(handler);
         }
 
-        void observe() {
+        @Override
+        protected void observe() {
+            super.observe();
             ContentResolver resolver = mContext.getContentResolver();
             resolver.registerContentObserver(SlimSettings.System.getUriFor(
                     SlimSettings.System.RECENT_PANEL_GRAVITY),
@@ -575,12 +578,7 @@ public class RecentController implements RecentPanelView.OnExitListener,
         }
 
         @Override
-        public void onChange(boolean selfChange) {
-            super.onChange(selfChange);
-            update();
-        }
-
-        public void update() {
+        protected void update() {
             // Close recent panel if it is opened.
             hideRecents(false);
 
