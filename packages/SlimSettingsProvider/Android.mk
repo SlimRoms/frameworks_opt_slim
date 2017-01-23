80b2d6e7cc72edacbf11de112aeefcc0747b1dce
Griffin Millender <griffinn.millender@gmail.com>
2017-01-23 16:56:26 -0600

    Rewrite SlimSettingsProvider based on AOSP SettingsProvider
    Change-Id: I66640d65aa26a97ca62c94fa00c1324a9d7ade25

--- packages/SlimSettingsProvider/Android.mk
+++ packages/SlimSettingsProvider/Android.mk
@@ -3,14 +3,10 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_SRC_FILES := $(call all-subdir-java-files)
+LOCAL_SRC_FILES := $(call all-subdir-java-files) \
+    src/org/slim/providers/settings/EventLogTags.logtags
 
-LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
-
-LOCAL_JAVA_LIBRARIES := \
-    telephony-common \
-    ims-common \
-    org.slim.framework
+LOCAL_JAVA_LIBRARIES := telephony-common ims-common org.slim.framework
 
 LOCAL_PACKAGE_NAME := SlimSettingsProvider
 LOCAL_CERTIFICATE := platform
