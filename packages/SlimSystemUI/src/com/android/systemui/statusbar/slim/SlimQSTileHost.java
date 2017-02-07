4488eebcc27f51a436f03246236a8888122f155b
fusionjack <dogfight60-fusionjack@yahoo.de>
2017-02-07 03:11:22 +0100

    Add Reboot/Recovery QS Tile
    Change-Id: I96968e25ebef20b07ba14e4fc096c6219a959cf9
    Signed-off-by: Josue Rivera <prbassplayer@gmail.com>

--- packages/SlimSystemUI/src/com/android/systemui/statusbar/slim/SlimQSTileHost.java
+++ packages/SlimSystemUI/src/com/android/systemui/statusbar/slim/SlimQSTileHost.java
@@ -24,6 +24,7 @@ import com.android.systemui.statusbar.policy.UserSwitcherController;
 import com.android.systemui.statusbar.policy.ZenModeController;
 
 import com.android.systemui.qs.tiles.CaffeineTile;
+import com.android.systemui.qs.tiles.RebootTile;
 import com.android.systemui.qs.tiles.SyncTile;
 
 public class SlimQSTileHost extends QSTileHost {
@@ -49,6 +50,8 @@ public class SlimQSTileHost extends QSTileHost {
         switch(tileSpec) {
             case "caffeine":
                 return new CaffeineTile(this);
+            case "reboot":
+                return new RebootTile(this);
             case "sync":
                 return new SyncTile(this);
             default:
