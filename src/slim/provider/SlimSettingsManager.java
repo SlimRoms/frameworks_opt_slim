83aba67d81e37d18e2fa11e583ef0102ace273d7
Griffin Millender <griffinn.millender@gmail.com>
2017-02-13 22:49:08 -0600

    Add SlimSettingsManager to handle settings changes    * inspired by TunerService
    Change-Id: I44d378611f4b1e7d3ffd2ea8de1bcf328e68edb4

---
+++ src/slim/provider/SlimSettingsManager.java
@@ -0,0 +1,60 @@
+package slim.provider;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.net.Uri;
+import android.util.Log;
+import android.os.IBinder;
+import android.os.ServiceManager;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import slim.constants.SlimServiceConstants;
+
+public class SlimSettingsManager {
+
+    private static SlimSettingsManager sInstance;
+    private static ISlimSettingsService sService;
+
+
+    private SlimSettingsManager() {
+    }
+
+    public static synchronized SlimSettingsManager getInstance() {
+        if (sInstance == null) {
+            sInstance = new SlimSettingsManager();
+        }
+        return sInstance;
+    }
+
+    public abstract static class OnSettingsChangedListener extends ISettingsChangedListener.Stub {
+    }
+
+    public void registerListener(OnSettingsChangedListener listener, Uri[] settingsUris) {
+        try {
+            getService().registerListener(listener, settingsUris);
+        } catch (Exception e) {}
+    }
+
+    void onChange(Uri uri, String value) {
+        try {
+            getService().onChange(uri, value);
+        } catch (Exception e) {}
+    }
+
+    private ISlimSettingsService getService() {
+        if (sService != null) {
+            return sService;
+        }
+        IBinder b = ServiceManager.getService(SlimServiceConstants.SLIM_SETTINGS_SERVICE);
+        if (b != null) {
+            sService = ISlimSettingsService.Stub.asInterface(b);
+            return sService;
+        }
+        return null;
+    }
+}
