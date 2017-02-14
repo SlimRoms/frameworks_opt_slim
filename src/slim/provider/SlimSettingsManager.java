ece68f680cd4ee9859fd4fd0d3b843f6449bf01e
Griffin Millender <griffinn.millender@gmail.com>
2017-02-13 22:49:08 -0600

    Add SlimSettingsManager to handle settings changes    * inspired by TunerService
    Change-Id: I44d378611f4b1e7d3ffd2ea8de1bcf328e68edb4

---
+++ src/slim/provider/SlimSettingsManager.java
@@ -0,0 +1,77 @@
+package slim.provider;
+
+import android.content.Context;
+import android.net.Uri;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class SlimSettingsManager {
+
+    private static SlimSettingsManager sInstance;
+    private final ContentResolver mResolver;
+
+    private final HashMap<OnSettingsChangedListener, List<Uri>> mListeners = new HashMap<>();
+
+    private SlimSettingsManager(ContentResolver resolver) {
+        mResolver = resolver;
+    }
+
+    public static synchronized SlimSettingsManager getInstance(ContentResolver resolver) {
+        if (sInstance == null) {
+            sInstance = new SlimSettingsManager(resolver);
+        }
+        return sInstance;
+    }
+
+    public interface OnSettingsChangedListener {
+        public void onChanged(String name, String value);
+    }
+
+    private String getString(Uri uri) {
+        List<String> segments = uri.getPathSegments();
+        String type = uri.get(0);
+        String key = uri.get(1);
+
+        if (type.equals("system")) {
+            return SlimSettings.System.getString(mResolver, key);
+        } else if (type.equals("secure")) {
+            return SlimSettings.Secure.getString(mResolver, key);
+        } else if (type.equals("global")) {
+            return SlimSettings.Global.getString(mResolver, key);
+        }
+        return null;
+    }
+
+    public void registerListener(OnSettingsChangedListener listener, Uri... settingsUris) {
+        synchronized (mListeners) {
+            List<Uri> uris = mListeners.get(listener);
+            if (uris == null) {
+                uris = new ArrayList<>();
+                mListeners.put(listener, uris);
+            }
+            for (String uri : settingsUris) {
+                uris.add(uri);
+                listener.onChanged(uri.getPathSegments().get(1), getString(uri));
+            }
+        }
+    }
+
+    void onChange(String key, String value) {
+        Log.d("TEST", "key=" + key + " : value=" + value);
+        synchronized (mListeners) {
+            final List<OnSettingsChangedListener> listeners = new ArrayList<>();
+            for (Map.Entry<OnSettingsChangedListener, List<String>> entry : mListeners.entrySet()) {
+                if (entry.getValue().contains(key)) {
+                    listeners.add(entry.getKey());
+                }
+            }
+            for (OnSettingsChangedListener listener : listeners) {
+                listener.onChanged(key, value);
+            }
+        }
+    }
+}
